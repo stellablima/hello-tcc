ae284acc703644ea629e51092da4d0ee2fc6f336
Stella <stellabelolima@gmail.com>
2021-10-23 20:22:10 -0300

    Adicionar nome procedimento teste

--- app/src/main/java/com/example/v1tcc/AdicionarProcedimento.java
+++ app/src/main/java/com/example/v1tcc/AdicionarProcedimento.java
@@ -2,19 +2,83 @@ package com.example.v1tcc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class AdicionarProcedimento extends AppCompatActivity {
+
+    private EditText edtNomeProcedimento;
+    private EditText edtDataProcedimento;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adicionar_procedimento);
+
+        edtNomeProcedimento = findViewById(R.id.edtNomeProcedimento);
+        edtDataProcedimento = findViewById(R.id.edtDataProcedimento);
     }
 
     public void btnSalvarProcedimentoOnClick(View view){
         Toast.makeText(this, "Salvo", Toast.LENGTH_SHORT).show();
+        try { preenchimentoValido();
+            //if (operacao == OP_INCLUI) {
+                insereEstq();
+            //}
+            //else if (operacao == OP_ALTERA) {
+            //    alteraEstq();
+            //    finish();
+            //}
+            finish();
+        } catch (SQLiteException e) {
+            Toast.makeText(this, "Inclusão falhou", Toast.LENGTH_LONG).show();
+        }
+        catch (Exception e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void insereEstq() {
+        BDRotinaHelper bdRotinaHelper = new BDRotinaHelper(this);
+        SQLiteDatabase bd = bdRotinaHelper.getWritableDatabase();
+        ContentValues cvEstq = carregaCVProcedimento();
+        bd.insert("PROCEDIMENTO", null, cvEstq);
+    }
+
+    private ContentValues carregaCVProcedimento() {
+        ContentValues cv = new ContentValues();
+        cv.put("NOME", edtNomeProcedimento.getText().toString());
+
+        //o que ele vai salvar no futuro no banco vai ser um alarme com uma configuração e uma peridiocidade, e o que vai
+        //persistir no banco é quando a ação for realizada os dados mínimos para relatório, toda a mágica fora o crud
+        //vai acontecer ali e pode ser necessário rependar melhor esse módulo, vai ser um tabelão de crescimento esponencial com indexes
+        //mas tbm pode filtrar gerar o relatório e apagar os dados mais antigos periodicamente
+        cv.put("DATA_PREVISAO", edtDataProcedimento.getText().toString());
+        //cv.put("UNID", spnUnidade.getSelectedItem().toString());
+        //cv.put("QTDE", qtde);
+        //cv.put("PCUNIT", pcUnit);
+
+
+        return cv;
+    }
+
+    private void preenchimentoValido() throws Exception {
+        String s;
+        s = edtNomeProcedimento.getText().toString();
+        if (s.equals(""))
+            throw new Exception("O Nome deve ser preenchido");
+        //else if (operacao == OP_INCLUI && jaExiste(s))
+        //    throw new Exception("Código já cadastrado");
+        //s = edtNome.getText().toString();
+        //if (s.equals(""))
+        //    throw new Exception("O Nome do Produto deve ser preenchido");
+        //s = spnUnidade.getSelectedItem().toString();
+        //if (s.equals(""))
+        //    throw new Exception("A Unidade deve ser preenchida");
     }
 }
