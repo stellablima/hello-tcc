d9902469e5fb9cbb9d690841197c4f91f7436185
Stella <stellabelolima@gmail.com>
2021-10-24 16:27:33 -0300

    Setando Alarme

--- app/src/main/java/com/example/v1tcc/AdicionarProcedimento.java
+++ app/src/main/java/com/example/v1tcc/AdicionarProcedimento.java
@@ -2,18 +2,33 @@ package com.example.v1tcc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.TimePickerDialog;
 import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
+import android.provider.AlarmClock;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
+
+import java.sql.Struct;
+import java.util.Calendar;
 
 public class AdicionarProcedimento extends AppCompatActivity {
 
     private EditText edtNomeProcedimento;
-    private EditText edtDataProcedimento;
+    private TextView txtHoraProcedimento;
+    private TimePicker tmpHoraAlarme;
+    private Calendar calHoraAlarm;
+    private String horaAtual;
+    private String minAtual;
+    private String horaSelecionadaAlarme = "11:11";
+    private TimePickerDialog mTimePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +36,77 @@ public class AdicionarProcedimento extends AppCompatActivity {
         setContentView(R.layout.activity_adicionar_procedimento);
 
         edtNomeProcedimento = findViewById(R.id.edtNomeProcedimento);
-        edtDataProcedimento = findViewById(R.id.edtDataProcedimento);
+        txtHoraProcedimento = findViewById(R.id.txtHoraProcedimento);
+    }
+
+    public void txtHoraProcedimentoOnClick(View view){
+        Toast.makeText(this, "clicado",Toast.LENGTH_LONG).show();
+
+        calHoraAlarm = Calendar.getInstance();
+        horaAtual = Integer.toString(calHoraAlarm.get(Calendar.HOUR_OF_DAY));
+        minAtual = Integer.toString(calHoraAlarm.get(Calendar.MINUTE));
+       /* tmpHoraAlarme = new TimePickerDialog(
+                this,
+                (TimePicker timePicker, int hourOfDay, int minutes) => {
+                    txtHoraProcedimento.setText(String.format("%02d", hourOfDay));
+                    txtHoraProcedimento.setText(String.format("%02d", hourOfDay));
+                },
+                horaAtual, minAtual, true);
+        tmpHoraAlarme.show();
+        );*/
+        //bom exemplo https://stackoverflow.com/questions/17901946/timepicker-dialog-from-clicking-edittext
+        //txtHoraProcedimento.setOnClickListener(new OnClickListener() {
+           // @Override
+            //public void onClick(View v) {
+                Calendar mcurrentTime = Calendar.getInstance();
+                int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+                int minute = mcurrentTime.get(Calendar.MINUTE);
+
+                mTimePicker = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                        txtHoraProcedimento.setText( selectedHour + ":" + selectedMinute);
+                    }
+                }, hour, minute, true);//tem como pegar o padrão corernte no dispositivo?
+                mTimePicker.setTitle("Select Time");
+                mTimePicker.show();
+           // }
+      //  });
+
+//        //mTimePicker.getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
+//        //    @Override
+//        //    public void onClick(View view) {
+//                String[] txtHora =  txtHoraProcedimento.getText().toString().split(":");
+//                Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM); //valor.substring(0,valor.length-2);
+//                intent.putExtra(AlarmClock.EXTRA_HOUR, Integer.parseInt(txtHora[0]));//(int) txtHoraProcedimento.getText().toString().indexOf(0,2));//int
+//                intent.putExtra(AlarmClock.EXTRA_MINUTES, Integer.parseInt(txtHora[1]));//txtHora.substring(3,5));// (int) txtHoraProcedimento.toString().indexOf(2,4));//int
+//                intent.putExtra(AlarmClock.EXTRA_MESSAGE,"Teste");
+//                if(intent.resolveActivity(getPackageManager())!=null)
+//                    startActivity(intent);
+//        //    }
+//        //});
+
     }
 
     public void btnSalvarProcedimentoOnClick(View view){
-        Toast.makeText(this, "Salvo", Toast.LENGTH_SHORT).show();
-        try { preenchimentoValido();
+
+        try {
+            preenchimentoValido();
+
+
+            String[] txtHora =  txtHoraProcedimento.getText().toString().split(":");
+            Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM); //valor.substring(0,valor.length-2);
+            intent.putExtra(AlarmClock.EXTRA_HOUR, Integer.parseInt(txtHora[0]));//(int) txtHoraProcedimento.getText().toString().indexOf(0,2));//int
+            intent.putExtra(AlarmClock.EXTRA_MINUTES, Integer.parseInt(txtHora[1]));//txtHora.substring(3,5));// (int) txtHoraProcedimento.toString().indexOf(2,4));//int
+
+
+            //TIPO|ID|NOME|um código aqui
+            intent.putExtra(AlarmClock.EXTRA_MESSAGE,edtNomeProcedimento.getText().toString());
+
+
+            if(intent.resolveActivity(getPackageManager())!=null)
+                startActivity(intent);
+
             //if (operacao == OP_INCLUI) {
                 insereEstq();
             //}
@@ -58,7 +138,7 @@ public class AdicionarProcedimento extends AppCompatActivity {
         //persistir no banco é quando a ação for realizada os dados mínimos para relatório, toda a mágica fora o crud
         //vai acontecer ali e pode ser necessário rependar melhor esse módulo, vai ser um tabelão de crescimento esponencial com indexes
         //mas tbm pode filtrar gerar o relatório e apagar os dados mais antigos periodicamente
-        cv.put("DATA_PREVISAO", edtDataProcedimento.getText().toString());
+        cv.put("DATA_PREVISAO", txtHoraProcedimento.getText().toString());
         //cv.put("UNID", spnUnidade.getSelectedItem().toString());
         //cv.put("QTDE", qtde);
         //cv.put("PCUNIT", pcUnit);
